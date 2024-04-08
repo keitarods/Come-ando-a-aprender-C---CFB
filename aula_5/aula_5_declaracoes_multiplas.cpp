92db9077ca8939f623d906b8d639c023a7ba0549
keitarods <83796605+keitarods@users.noreply.github.com>
2024-04-07 22:08:25 -0300

    Declaracoes multiplas de variaveis, contantes #Define

---
+++ aula_5/aula_5_declaracoes_multiplas.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+#define pi 3.1415
+#define canal cout << "Canal Fessor Bruno\n\n";
+
+int main(){
+	
+	int vidas=3, tiros=500, life=1000;
+	
+	cout << pi << "\n\n";
+	canal	
+
+	
+	return 0;
+}
