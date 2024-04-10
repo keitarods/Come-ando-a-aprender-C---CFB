07e343a9c12cc21fab7da44d90c153bbbb7076df
keitarods <83796605+keitarods@users.noreply.github.com>
2024-04-09 23:32:30 -0300

    Incremento e decremento de variaveis

---
+++ aula_7/aula_7_incremento_decremento_variaveis.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	
+	//n1 = n1+10	n1+=10;
+	//n1 = n1-10	n1-=10;
+	//n1 = n1*10	n1*=10;
+	//n1 = n1/10	n1/=10;
+	//n1++			n1--;
+	//n1++ pré fixado			++n1; #pos fixado
+	
+	int n1=10;
+	
+	
+	cout << n1-- << "\n\n";
+	cout << n1 << "\n\n";
+	
+	return 0;
+}
