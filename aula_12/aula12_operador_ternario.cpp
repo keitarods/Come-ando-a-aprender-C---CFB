f342e239e571cab233a74a997837fbf4a3cbafaa
keitarods <83796605+keitarods@users.noreply.github.com>
2024-04-09 23:35:05 -0300

    Aula 12 - Operador Ternario

---
+++ aula_12/aula12_operador_ternario.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	
+	int n1, x;
+	
+	cout << "O valor de n1 é: ";
+	cin >> n1;
+	
+	
+	x = 5;
+	
+	(n1 >=60) ? x++ : x--;
+	
+	cout << x;
+	
+}
