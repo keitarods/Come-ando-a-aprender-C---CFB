3d096eb1a8b2219a9395f908f66ae98f70c0ea9b
keitarods <83796605+keitarods@users.noreply.github.com>
2024-04-11 13:58:13 -0300

    aula_14_comando_while

---
+++ aula_14/aula_14_comando_while.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	
+	int cont;
+	
+	cont = 0;
+	
+	while(cont <=20){
+		cout << "Keitaro esta estudando C++" << " - " << cont << "\n";
+		cont++;
+	}
+	
+	return 0;
+}
