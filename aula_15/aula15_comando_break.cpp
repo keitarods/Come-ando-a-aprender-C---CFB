658be3f0b5e28875fc6106c49e3b0e35ae58842e
keitarods <83796605+keitarods@users.noreply.github.com>
2024-04-15 23:23:02 -0300

    Adicionando arquivos das aulas 15 - break e aula 16 - do_while

---
+++ aula_15/aula15_comando_break.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int cont;
+
+    while (cont <1000){
+        cout << cont << '\n';
+        cont ++;
+        if (cont == 500){
+            break;
+        }
+    }
+
+
+	return 0;
+
+}
---
+++ aula_16/aula_16_comando_do_while.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int cont;
+
+	cont = 20;
+
+	do{
+        cout << "Testando o comando do While\n";
+	} while(cont<20);
+
+	return 0;
+
+}
