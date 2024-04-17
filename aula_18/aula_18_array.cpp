b9af598298428bb9ac0ec5651011f15069efe46a
keitarods <83796605+keitarods@users.noreply.github.com>
2024-04-16 23:14:05 -0300

    Aula-18-Array

---
+++ aula_18/aula_18_array.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+	int tam = 5;
+	int vetor[tam]={10,20,30,40,50};
+	int i;
+
+    for(i=0; i<5; i++){
+        cout << vetor[i] << "\n";
+    }
+
+	return 0;
+
+}
