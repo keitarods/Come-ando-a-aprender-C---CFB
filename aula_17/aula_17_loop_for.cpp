979197ed639f0ba206660ef888543abfcfd9264d
keitarods <83796605+keitarods@users.noreply.github.com>
2024-04-16 23:13:35 -0300

    Aula-17-loop-for

---
+++ aula_17/aula_17_loop_for.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+	int x;
+
+	//for(ini; cond; inc/dec);
+
+	x=0;
+
+	for (x; x<100; x++){
+        cout << "O valor de X é: " << x << "\n";
+	}
+
+	return 0;
+
+}
