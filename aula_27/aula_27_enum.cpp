25e1a211f5e7a790855e962199cd444257a8a947
keitarods <83796605+keitarods@users.noreply.github.com>
2024-04-22 13:54:10 -0300

    Aula_27_enum

---
+++ aula_27/aula_27_enum.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    enum armas{Rifle = 30, Sniper = 10, Escopeta = 7, Carabina = 8};
+
+    armas armasel;
+
+    armasel=Sniper;
+
+    cout << armasel;
+
+	return 0;
+
+}
