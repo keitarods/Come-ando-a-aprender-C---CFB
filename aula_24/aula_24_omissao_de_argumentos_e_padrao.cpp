ddf31c90b8e40f64c61d2527f9ba68ef846a5616
keitarods <83796605+keitarods@users.noreply.github.com>
2024-04-22 07:57:33 -0300

    aula-24-omissao-de-argumentos-e-padroes

---
+++ aula_24/aula_24_omissao_de_argumentos_e_padrao.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+void texto(string txt="Keitaro");
+
+int main(){
+
+    texto("Ola Blz?");
+
+
+	return 0;
+
+}
+
+void texto(string txt){
+    cout << txt << "\n";
+}
