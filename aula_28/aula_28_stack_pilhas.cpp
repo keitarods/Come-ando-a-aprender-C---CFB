77971233c74640b6f2ef3f9a5e872930d11d0705
keitarods <83796605+keitarods@users.noreply.github.com>
2024-04-22 13:54:40 -0300

    Aula_28_stack_pilhas

---
+++ aula_28/aula_28_stack_pilhas.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+int main(){
+
+    stack <string> cartas;
+
+    cartas.push("Rei de copas");
+    cartas.push("Rei de espadas");
+    cartas.push("Rei de paus");
+    cartas.push("Rei de ouros");
+
+    cout << "Tamanho da pilha: " << cartas.size() << "\n";
+
+    cout << "Carta do topo" << cartas.top() << "\n";
+
+    cartas.pop();
+    cartas.pop();
+
+    cout << "Carta do topo" << cartas.top() << "\n";
+
+	return 0;
+
+}
